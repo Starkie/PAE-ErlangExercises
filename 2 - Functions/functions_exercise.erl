369979f4354fdf4a3d040a6a4748d7dca7d44bfb
Adriano Vega Llobell <adrianovega@hotmail.com>
2020-03-07 21:53:50 +0100

    Solved exercise 3: Function that returns different parts of a list depending of a starting tuple.

--- 2 - Functions/functions_exercise.erl
+++ 2 - Functions/functions_exercise.erl
@@ -1,5 +1,5 @@
 -module(functions_exercise).
--export([ord/1, igual/1]).
+-export([ord/1, igual/1, foo/1]).
 
 %% 1) Escribid una función que devuelva el ordinal de un número en inglés (considera sólo números del 1 al 9).
 %% Por ej.,
@@ -27,3 +27,14 @@ lastDigitOfNumber(Num) -> list_to_integer([lists:last(integer_to_list(Num))]).
 %% igual({a,a}) -> true
 igual({X, X}) -> true;
 igual(_) -> false.
+
+%%3) Escribid una función que tome una lista cuyo primer elemento es una tupla de dos elementos.
+%%   Si los dos elementos son iguales, debe devolver una lista con el resto de elementos.
+%%   En caso contratio, debe devolver la tupla pero intercambiando los elementos de posición.
+%%
+%% Por ej.,
+%% foo([{4,4},a,b,c]) -> [a,b,c]
+%% foo([{3,4},a,b,c]) -> {4,3}
+foo([{X, X} | Tail]) -> Tail;
+foo([{X, Y} | _]) -> {Y, X};
+foo(X) -> X.
